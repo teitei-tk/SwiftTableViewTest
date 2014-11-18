79de88d3a9eaae629971743604aba64df56e7b1a
teitei-tk <teitei.tk@gmail.com>
2014-11-18 11:48:47 +0900

    apply json data

--- TableCellTest/ViewController.swift
+++ TableCellTest/ViewController.swift
@@ -48,15 +48,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
         for json in json_data {
             let (_, data) = json
-            println(data["id"])
-            println(data["source_url"])
-//          self.tableCells.append(Cell(name: data["id"], imageName: data["source_url"]))
+            self.tableCells.append(Cell(name: data["id"].string!, imageName: data["source_url"].string!))
         }
 
-        for cellName in ["hoge", "fuga"] {
-            self.tableCells.append(Cell(name: cellName, imageName: "test_image"))
-        }
-  
         dispatch_async(dispatch_get_main_queue(), {
             self.tableView.reloadData()
         })
